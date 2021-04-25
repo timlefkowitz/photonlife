97ec76342f668c5375065eb55b470fbc1ee649a9
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-04-24 23:36:23 -0500

    Feat: updates

---
+++ js/keys.js
@@ -0,0 +1,5 @@
+var mapboxtoken = "pk.eyJ1IjoidGltb3RoeWxlZmtvd2l0eiIsImEiOiJja25ham5vNDMxaWJjMnZsZ2RiOHpyd3Z4In0._djH9icZ5-VN_k6R9r69ug"
+
+var OPEN_WEATHER_APPID = "d08a73d83534c3d0250817519765b1a5";
+
+var gittoken = "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";
